8aab9e002b6d9554ccf0f8f885c582ed41816ebf
Erik Cleyverton <erik.c.correia@aluno.senai.br>
2026-08-03 21:09:02 -0300

    Criado projeto angular

--- layout/src/app/layout/layout.ts
+++ layout/src/app/layout/layout.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [FormsModule], 
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+
+  nome_pessoa: string = '';
+  email_pessoa: string = '';
+  numero_cpf: string = '';
+  data_nascimento: string = '';
+  numero_cep: string = '';
+  municipio: string = '';     
+  sigla_uf: string = '';
+  sigla_municipio: string = '';
+
+  LimparItem(): void {
+    this.nome_pessoa = '';
+    this.email_pessoa = '';
+    this.numero_cpf = '';
+    this.data_nascimento = '';
+    this.numero_cep = '';
+    this.municipio = '';
+    this.sigla_uf = '';
+  }
+}
